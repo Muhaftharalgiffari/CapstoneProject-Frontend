a59c58c6de2b2e5fc0fc29d5057d0db5fc41c592
Sirnerok <afthar05@gmail.com>
2025-05-15 15:35:02 +0800

    chore: hapus Procfile agar Railway deteksi Node.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { spawn } = require('child_process');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
